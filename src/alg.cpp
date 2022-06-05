d361078c8679ec0167093c643162ca594cdae062
UmerovEmran <99113996+UmerovEmran@users.noreply.github.com>
2022-06-05 22:48:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,10 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
+  std::string qwe = tree.storage(n - 1);
+  std::vector<char> result;
+  for (int m = 0; m < qwe.size(); ++m) {
+      result.push_back(qwe[m]);
+  }
+  return result;
 }
